a09e2ee67de7cc1a837959cb28791b33e7b971ec
Ryan Martin <iamatallone@gmail.com>
2023-11-03 23:22:40 -0400

    fix warning

--- js/calculations.js
+++ js/calculations.js
@@ -3,10 +3,10 @@ let UKPound = new Intl.NumberFormat('en-GB', {
     currency: 'GBP',
 });
 function calcBid() {
-    let carValue = 0;
-    let buyerPrem = 0;
-    carValue = document.getElementById('carValue').value;
-    buyerPrem = document.getElementById('buyerPrem').value;
+    //let carValue = 0;
+    //let buyerPrem = 0;
+    let carValue = document.getElementById('carValue').value;
+    let buyerPrem = document.getElementById('buyerPrem').value;
     let prem = ((buyerPrem * 1.2)/100)+1;
     console.log(prem);
     document.getElementById('price').innerHTML = UKPound.format(carValue * prem);
