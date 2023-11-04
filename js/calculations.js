4b4171858e80e7bebfbe0d13fb159375d6642da4
Ryan Martin <iamatallone@gmail.com>
2023-11-04 00:50:32 -0400

    Commit. VAT working and others mostly done.

--- js/calculations.js
+++ js/calculations.js
@@ -7,24 +7,133 @@ function calcBid() {
     //let buyerPrem = 0;
     let carValue = document.getElementById('carValue').value;
     let buyerPrem = document.getElementById('buyerPrem').value;
-    let prem = ((buyerPrem * 1.2)/100)+1;
-    console.log(prem);
+    let prem = "";
+    if(findChecked("vat") == "vat") {
+        prem = ((buyerPrem * 1.2)/100)+1;
+    } else {
+        prem = (buyerPrem/100)+1;
+    }
+    console.log(findChecked("vat"));
+    //console.log(getSelected("auctionHouse"));
     document.getElementById('price').innerHTML = UKPound.format(carValue * prem);
     document.getElementById('buyprem').innerHTML = UKPound.format((carValue * prem) - carValue);
 }
 
-function ACA() {
-    //
+function findChecked(elementName) {
+    let radio= document.getElementsByName(elementName);
+    let checked = "";
+    for(i=0; i<radio.length; i++) {
+        if(radio[i].checked)
+            checked = radio[i].value;
+    }
+    return checked;
 }
 
-function mathewsons() {
-    //
+function setBuyerPrem(selectedAuctionHouse) {
+    switch (selectedAuctionHouse) {
+        case "ACA":
+            document.getElementById('buyerPrem').value = 8;
+            document.getElementById('buyerPrem').disabled = false;
+            break;
+        case "mathewsons":
+            document.getElementById('buyerPrem').value = 10;
+            document.getElementById('buyerPrem').disabled = false;
+            break;
+        case "countingcars":
+            document.getElementById('buyerPrem').value = 6;
+            document.getElementById('buyerPrem').disabled = false;
+            break;
+        case "candc":
+            document.getElementById('buyerPrem').value = 0;
+            document.getElementById('buyerPrem').disabled = true;
+            break;
+        default:
+            document.getElementById('buyerPrem').disabled = false;
+            break;
+    }
 }
 
-function countingcars() {
-    //
+function getSelected(elementName) {
+    return document.getElementById(elementName).value;
 }
 
-function other() {
-    //
+function ACA(carValue, buyerPrem) {
+    //returns array(TOTAL, BUYERS_PREMIUM)
+    let ret = array();
+    let bpTotal = carValue * prem;
+    if(bpTotal < 192) {
+        bpTotal = 192;
+    }
+    if(findChecked("vat") == "vat") {
+        let prem = (buyerPrem/100)+1;
+    } else {
+        let prem = ((buyerPrem * 1.2)/100)+1;
+    }
+    ret[0] = carValue * prem;
+    ret[1] = prem;
+
+    return ret;
+}
+
+function mathewsons(carValue, buyerPrem) {
+    //returns array(TOTAL, BUYERS_PREMIUM)
+    let ret = array();
+    let prem1 = 0;
+    let prem2 = 0;
+    let prem3 = 0;
+    let r = 0;
+    let p = 0;
+
+    if(findChecked("vat") == "vat") {
+        prem1 = (10/100)+1;
+        prem2 = (7.5/100)+1;
+        prem3 = (5/100)+1;
+    } else {
+        prem1 = ((10 * 1.2)/100)+1;
+        prem2 = ((7.5 * 1.2)/100)+1;
+        prem3 = ((5 * 1.2)/100)+1;
+    }
+
+    if (carValue <= 2000) {
+        r = carValue * prem1;
+    } else if(carValue > 2000 && carValue <= 20000) {
+        r = carValue * prem2;
+    } else if(carValue > 20000) {
+        r = (20000 * prem2) + ((carValue - 20000) * prem3);
+    }
+
+    ret[0] = r;
+    ret[1] = r - carValue;
+
+    return ret;
+}
+
+function countingcars(carValue, buyerPrem) {
+    //returns array(TOTAL, BUYERS_PREMIUM)
+    let ret = array();
+    if(findChecked("vat") == "vat") {
+        let prem = (buyerPrem/100)+1;
+    } else {
+        let prem = ((buyerPrem * 1.2)/100)+1;
+    }
+
+    ret[0] = carValue * prem;
+    ret[1] = prem;
+
+    return ret;
+}
+
+function other(carValue, buyerPrem) {
+    //returns array(TOTAL, BUYERS_PREMIUM)
+    let ret = array();
+    if(findChecked("vat") == "vat") {
+        let prem = (buyerPrem/100)+1;
+    } else {
+        let prem = ((buyerPrem * 1.2)/100)+1;
+    }
+
+    ret[0] = carValue * prem;
+    ret[1] = prem;
+
+    return ret;
 }
